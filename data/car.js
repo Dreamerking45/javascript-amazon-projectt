be47926f7a0998ff9dd3c3f1dfc6d66eec78a4c2
Dreamerking45 <adrianwariero1@gmail.com>
2025-02-07 15:19:54 +0300

    Updated appliance class in products.js and added private properties in car.js

--- data/car.js
+++ data/car.js
@@ -1,19 +1,21 @@
 export class Car {
-  brand;
-  model;
+  #brand;
+  #model;
   speed = 0;
-  isTrunkOpen;
-  constructor(brand, model) {
-    this.brand = brand;
-    this.model = model;
+  isTrunkOpen = false;
+  constructor(carDetails) {
+    this.#brand = carDetails.brand;
+    this.#model = carDetails.model;
   };
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}, speed: ${this.speed} km/h`)
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+
+    console.log(`${this.#brand} ${this.#model}, speed: ${this.speed} km/h, Trunk: ${trunkStatus}`)
   }
 
   go() {
-    if (this.isTrunkOpen = false && this.speed <= 195) {
+    if (this.isTrunkOpen === false && this.speed <= 195) {
       return this.speed += 5
     } 
   }
@@ -23,17 +25,47 @@ export class Car {
     }
   }
   openTrunk() {
-    
+    if (this.speed === 0) {
+      this.isTrunkOpen = true
+    }
   }
   closeTrunk() {
+    this.isTrunkOpen = false;
+  }
 
 
+}
+class RaceCar extends Car {
+  acceleration;
+
+  constructor(carDetails) {
+    super(carDetails)
+    this.acceleration = carDetails.acceleration
+  }
+  go() {
+    this.speed += this.acceleration
+
+    if (this.speed > 300) {
+      this.speed = 300;
+    }
+  }
+  openTrunk() {
+    console.log('Race cars do not have a trunk')
+  }
+  closeTrunk() {
+    console.log('Race cars do not have a trunk')
+  }
+
+  go() {
+    if (this.isTrunkOpen === false && this.speed < 300) {
+      return this.speed += this.acceleration
+    } 
   }
 
 }
-const car1 = new Car('Toyota', 'Corolla')
-const car2 = new Car('Tesla', 'Model 3')
-
+const car1 = new Car({brand: 'Toyota',model: 'Corolla'})
+const car2 = new Car({brand: 'Tesla', model: 'Model 3'})
+const racecar = new RaceCar({brand: 'McLaren', model: 'F1', acceleration: 20})
 console.log(car1)
 console.log(car2)
 car1.go()
@@ -41,3 +73,24 @@ car1.go()
 car2.go()
 car1.displayInfo()
 car2.displayInfo()
+car1.brake()
+car1.displayInfo()
+car1.openTrunk()
+car1.displayInfo()
+car1.go()
+car1.openTrunk()
+car1.closeTrunk()
+car1.go();
+car1.go()
+car1.displayInfo()
+car2.go()
+car2.go()
+car2.go()
+car2.displayInfo()
+racecar.go()
+racecar.go()
+racecar.go()
+racecar.displayInfo()
+racecar.openTrunk()
+racecar.brake()
+racecar.displayInfo()
--- data/products.js
+++ data/products.js
@@ -53,6 +53,22 @@ class Clothing extends Product {
     `;
   };
 }
+class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+  extraInfoHTML(){
+    return `
+     <a href="${this.instructionsLink}" target="_blank">Instructions chart</a>
+     <a href="${this.warrantyLink}" target="_blank">Warranty</a>
+     `
+  }
+}
 /*
 const date = new Date();
 console.log(date);
@@ -141,7 +157,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliances",
+    instructionsLink:"images/appliance-instructions.png" ,
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -330,7 +349,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: "appliances",
+    instructionsLink:"images/appliance-instructions.png" ,
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -699,7 +721,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliances",
+    instructionsLink:"images/appliance-instructions.png" ,
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -783,6 +808,8 @@ export const products = [
 ].map((productDetails)=>{
   if (productDetails.type === 'clothing') {
     return new Clothing(productDetails)
+  } else if(productDetails.type === 'appliances') {
+    return new Appliance(productDetails)
   }
   return new Product(productDetails)
 })
