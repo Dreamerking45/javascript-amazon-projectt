132a243e34650b73593bd4d6a3e8acbe9ec42244
Dreamerking45 <adrianwariero1@gmail.com>
2025-02-08 10:56:29 +0300

    learnet promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,14 +5,51 @@ import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
 import "../data/car.js"
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(() => {
+      resolve('value1');
+    })
+  }), 
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('value2');
+    });
+  })
+]).then((values) => {
+  console.log(values)
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
+new Promise((resolve)=>{
+  loadProducts(() => {
+    resolve();
+  })
+}).then(()=>{
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+}).then(()=>{
   renderOrderSummary();
   renderPaymentSummary();
 })
-;
+
+
+loadProducts(()=>{
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
 
   let today1 = dayjs();
   console.log(isSatSun(today1));
