edcbc8ad53b83e1cb700b46e6e585880bc4519b0
Dreamerking45 <adrianwariero1@gmail.com>
2025-02-08 11:53:32 +0300

    convert loadproducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,17 +4,13 @@ import isSatSun from "./checkWeekend.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 import "../data/car.js"
 
 Promise.all([
-  new Promise((resolve)=>{
-    loadProducts(() => {
-      resolve('value1');
-    })
-  }), 
+  loadProductsFetch(), 
   new Promise((resolve)=>{
     loadCart(()=>{
       resolve('value2');
